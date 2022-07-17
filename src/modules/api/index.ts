7c00b96261042b6856d63d7d74cfcb55625b785f
D0ugins <yusufishabazz@gmail.com>
2022-07-16 20:03:14 -0500

    Add evidenceBucket resolver

--- src/modules/api/index.ts
+++ src/modules/api/index.ts
@@ -1,12 +1,12 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
-import { EvidenceResolver, FileResolver } from './resolvers';
+import { EvidenceBucketResolver, EvidenceResolver, FileResolver, RoundResolver } from './resolvers';
 
 const PORT = process.env.API_PORT || 4000;
 async function main() {
   const schema = await buildSchema({
-    resolvers: [EvidenceResolver, FileResolver],
+    resolvers: [EvidenceResolver, FileResolver, RoundResolver, EvidenceBucketResolver],
   });
 
   const server = new ApolloServer({ schema });
--- src/modules/api/models/index.ts
+++ src/modules/api/models/index.ts
@@ -2,3 +2,4 @@ export * from './evidence';
 export * from './evidenceSet';
 export * from './file';
 export * from './round';
+export * from './evidenceBucket';
