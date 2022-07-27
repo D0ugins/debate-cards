88fb7bb8055622b8fc25c5a09956a1ed704a6d34
D0ugins <yusufishabazz@gmail.com>
2022-07-26 20:33:10 -0500

    Improve organization of query code

--- src/modules/api/resolvers/evidence.ts
+++ src/modules/api/resolvers/evidence.ts
@@ -13,15 +13,30 @@ const EvidenceGetResolver = createGetResolver('evidence', Evidence);
 
 @Resolver()
 export class EvidenceResolver extends EvidenceGetResolver {
-  // A lot of things are hard coded at the moment, in the future could be made customizable
   @Query((returns) => [Evidence])
   async search(
     @Args() { query, size, fields, evidenceSets, duplicateWeight }: EvidenceSearchArgs,
     @Info() info: GraphQLResolveInfo,
   ): Promise<Partial<Evidence>[]> {
+    const searchQuery = {
+      query_string: {
+        query,
+        type: 'most_fields', // sums scores from each field
+        fields: fields.map(({ field, weight }) => `${SearchableEvidenceField[field]}^${weight}`),
+      },
+    } as const;
+    const scoreBoost = {
+      script_score: {
+        script: {
+          params: { duplicateWeight },
+          source: "_score * Math.pow(doc['EvidenceBucket.count'].value, params.duplicateWeight)",
+        },
+      },
+    } as const;
+    const filter = evidenceSets ? { terms: { 'File.evidenceSetId': evidenceSets } } : undefined;
+
     // Does a search in elastic that only returns ids, then queries those ids from postgres
     // Not sure if this is the best approach but otherwise it would be more difficult to do relational queries
-    console.time('search');
     const results = await elastic.search({
       index: 'evidence',
       size,
@@ -29,36 +44,19 @@ export class EvidenceResolver extends EvidenceGetResolver {
         bool: {
           must: {
             function_score: {
-              query: {
-                query_string: {
-                  query,
-                  type: 'most_fields', // sums scores from each field
-                  fields: fields.map(({ field, weight }) => `${SearchableEvidenceField[field]}^${weight}`),
-                },
-              },
-              functions: [
-                {
-                  script_score: {
-                    script: {
-                      params: { duplicateWeight },
-                      source: "_score * Math.pow(doc['EvidenceBucket.count'].value, params.duplicateWeight)",
-                    },
-                  },
-                },
-              ],
+              query: searchQuery,
+              functions: [scoreBoost],
               boost_mode: 'replace',
             },
           },
-          filter: evidenceSets ? { terms: { 'File.evidenceSetId': evidenceSets } } : undefined,
+          filter,
         },
       },
-      collapse: {
-        field: 'bucketId',
-      },
+      collapse: { field: 'bucketId' },
       _source: false,
       docvalue_fields: ['id'],
     });
-    console.timeEnd('search');
+
     const ids: number[] = flatMap(results.hits.hits, 'fields.id');
     const evidence = await db.evidence.findMany({ where: { id: { in: ids } }, select: selectFields(info) });
     // Resort results based on ranking
